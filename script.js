45486f9f94eaeeecc1c5f66d8016d0a53cee9503
RaymONT <konasr6@gmail.com>
2022-04-21 16:35:43 +0300

    fix

--- script.js
+++ script.js
@@ -118,21 +118,21 @@ headerObserver.observe(header);
 
 /////////
 // Appearance  sections
-// const appearanceSection = function (entries, observer) {
-//   const entry = entries[0];
-//   if (!entry.isIntersecting) return;
-//   entry.target.classList.remove("section--hidden");
-//   observer.unobserve(entry.target);
-// };
-// const sectionObserver = new IntersectionObserver(appearanceSection, {
-//   root: null,
-//   threshold: 0.1,
-// });
+const appearanceSection = function (entries, observer) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(appearanceSection, {
+  root: null,
+  threshold: 0.1,
+});
 
-// allSection.forEach(section => {
-//   sectionObserver.observe(section);
-//   section.classList.add("section--hidden");
-// });
+allSection.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
 
 /////////
 // Lazy Loading
